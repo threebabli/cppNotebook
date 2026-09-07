ca7997f5edbb19584b08b16062d2ceb1e4910759
threebabli <interact.babli@gmail.com>
2026-09-07 21:23:44 +0530

    constant

--- basics/004.cpp
+++ basics/004.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main()
 {
-    const float pi= 3.14;    //for constant value declaration and initialize at the same time
+    const float pi= 3.14;    //for constant value declaration and initialize is done at the same time
     
     cout << "the value of pi is =" << pi;
 
